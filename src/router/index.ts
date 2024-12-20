c22dc4df3bf4a91191083e6c33f852cb78d0e02a
unknown <rijal.1344@gmail.com>
2024-12-20 13:24:24 +0700

    setup: add nprogress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginIndexView from '@/views/login/IndexView.vue'
 import DashboardIndexView from '@/views/dashboard/IndexView.vue'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,15 @@ const router = createRouter({
       component: DashboardIndexView,
     },
   ],
+})
+
+router.beforeEach(async (to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
