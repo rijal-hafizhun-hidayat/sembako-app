e3ff890f9f8e69c3a69427a4ef457af4b88cc192
unknown <rijal.1344@gmail.com>
2024-12-20 13:09:56 +0700

    page: add login page

---
+++ src/components/base/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="text-xl text-center">SEMBAKO APP</h1>
+</template>
--- src/views/login/IndexView.vue
+++ src/views/login/IndexView.vue
@@ -1,6 +1,35 @@
 <script setup lang="ts">
-console.log(true)
+import GuestLayout from '@/layouts/GuestLayout.vue'
+import InputLabel from '@/components/base/InputLabel.vue'
+import TextInput from '@/components/base/TextInput.vue'
+import PrimaryButton from '@/components/base/PrimaryButton.vue'
+import { reactive, ref, type Ref } from 'vue'
+
+interface Form {
+  email: string
+  password: string
+}
+
+const isLoading: Ref<boolean> = ref(false)
+const form: Form = reactive({
+  email: '',
+  password: '',
+})
 </script>
 <template>
-  <p class="text-red-500">hello world</p>
+  <GuestLayout>
+    <form class="space-y-4">
+      <div>
+        <InputLabel>email</InputLabel>
+        <TextInput type="email" class="mt-1 block w-full" v-model="form.email" />
+      </div>
+      <div>
+        <InputLabel>password</InputLabel>
+        <TextInput type="password" class="mt-1 block w-full" v-model="form.password" />
+      </div>
+      <div class="flex justify-end">
+        <PrimaryButton :disabled="isLoading" type="submit">login</PrimaryButton>
+      </div>
+    </form>
+  </GuestLayout>
 </template>
