33771a643fadb509358df31e3e2139b015741483
unknown <rijal.1344@gmail.com>
2024-12-23 14:09:02 +0700

    page: add form create role

--- src/views/role/CreateView.vue
+++ src/views/role/CreateView.vue
@@ -1,5 +1,22 @@
 <script setup lang="ts">
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
+import TextInput from '@/components/base/TextInput.vue'
+import InputLabel from '@/components/base/InputLabel.vue'
+import PrimaryButton from '@/components/base/PrimaryButton.vue'
+import { reactive, ref, type Ref } from 'vue'
+
+interface Form {
+  name: string
+}
+
+const isLoading: Ref<boolean> = ref(false)
+const form: Form = reactive({
+  name: '',
+})
+
+const send = () => {
+  console.log(form)
+}
 </script>
 <template>
   <DashboardLayout>
@@ -10,5 +27,19 @@ import DashboardLayout from '@/layouts/DashboardLayout.vue'
         </div>
       </div>
     </template>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="bg-white mt-10 px-4 py-6 rounded shadow-md overflow-x-auto">
+        <form @submit.prevent="send()" class="space-y-4">
+          <div>
+            <InputLabel>name</InputLabel>
+            <TextInput class="block w-full mt-1" v-model="form.name" />
+          </div>
+          <div>
+            <PrimaryButton :disabled="isLoading" type="submit">submit</PrimaryButton>
+          </div>
+        </form>
+      </div>
+    </div>
   </DashboardLayout>
 </template>
