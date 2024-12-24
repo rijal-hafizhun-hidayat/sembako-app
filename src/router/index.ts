378d451bdebeebe3072685427ff79b33167af0d3
unknown <rijal.1344@gmail.com>
2024-12-24 10:03:38 +0700

    page: add category item page

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import DashboardIndexView from '@/views/dashboard/IndexView.vue'
 import RoleIndexView from '@/views/role/IndexView.vue'
 import RoleCreateView from '@/views/role/CreateView.vue'
 import RoleShowView from '@/views/role/ShowView.vue'
+import CategoryItemIndexView from '@/views/category-item/IndexView.vue'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import { useAuthStore } from '@/stores/auth'
@@ -49,6 +50,16 @@ const router = createRouter({
         requiresAuth: true,
         requiresRoles: ['admin'],
       },
+    },
+    {
+      path: '/category-item',
+      children: [
+        {
+          path: '',
+          name: 'category-item.index',
+          component: CategoryItemIndexView,
+        },
+      ],
     },
   ],
 })
