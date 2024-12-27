56a1fb9972d82a99dafc5375f0c8b7725c0b0851
unknown <rijal.1344@gmail.com>
2024-12-27 08:01:03 +0700

    feat: add search role by name

--- src/views/role/IndexView.vue
+++ src/views/role/IndexView.vue
@@ -2,10 +2,11 @@
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
 import PrimaryButton from '@/components/base/PrimaryButton.vue'
 import DangerButton from '@/components/base/DangerButton.vue'
+import TextInput from '@/components/base/TextInput.vue'
 import api from '@/plugins/api'
 import { Timestamp } from '@/utils/timestamp'
 import type { AxiosError, AxiosResponse } from 'axios'
-import { onMounted, ref, type Ref } from 'vue'
+import { computed, onMounted, ref, type Ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { SweetAlert } from '@/utils/sweetalert'
 
@@ -22,6 +23,7 @@ interface Role {
 }
 
 const router = useRouter()
+const search: Ref<string> = ref('')
 const isLoading: Ref<boolean> = ref(false)
 const isLoadingButton: Ref<boolean> = ref(false)
 const roles: Ref<Role[]> = ref([])
@@ -67,6 +69,11 @@ const toShowRoleView = (roleId: number) => {
     },
   })
 }
+
+const searchRoleByName = computed(() => {
+  const query = search.value.toLowerCase()
+  return roles.value.filter((role) => role.name.toLowerCase().includes(query))
+})
 </script>
 <template>
   <DashboardLayout>
@@ -83,6 +90,16 @@ const toShowRoleView = (roleId: number) => {
 
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="bg-white mt-10 px-4 py-6 rounded shadow-md overflow-x-auto">
+        <div class="grid grid-rows-1 sm:grid-cols-2 gap-3">
+          <div>
+            <TextInput v-model="search" placeholder="find role by name" class="block w-full" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="bg-white mt-10 px-4 py-6 rounded shadow-md overflow-x-auto">
         <table class="w-full whitespace-nowrap">
           <thead>
             <tr class="text-left font-bold">
@@ -94,7 +111,7 @@ const toShowRoleView = (roleId: number) => {
             </tr>
           </thead>
           <tbody v-if="roles.length > 0">
-            <tr v-for="(role, index) in roles" :key="role.id" class="hover:bg-gray-100">
+            <tr v-for="(role, index) in searchRoleByName" :key="role.id" class="hover:bg-gray-100">
               <td class="border-t items-center px-6 py-4">
                 {{ index + 1 }}
               </td>
