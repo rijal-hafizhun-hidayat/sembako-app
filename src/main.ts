7df4e1d4b912510be628ecff97677fa6b2aeb2b0
unknown <rijal.1344@gmail.com>
2025-01-04 08:32:52 +0700

    chart: integrate apexchart in dashboard page

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import VueApexCharts from 'vue3-apexcharts'
+
 const app = createApp(App)
 
+app.use(VueApexCharts)
 app.use(createPinia())
 app.use(router)
 
--- src/views/dashboard/IndexView.vue
+++ src/views/dashboard/IndexView.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
 import { useAuthStore } from '@/stores/auth'
+import { ref } from 'vue'
 
 const authStore = useAuthStore()
+const options = ref({
+  options: {
+    chart: {
+      id: 'vuechart-example',
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+    },
+  },
+})
+const series = ref([
+  {
+    name: 'series-1',
+    data: [30, 40, 45, 50, 49, 60, 70, 91],
+  },
+])
 </script>
 <template>
   <DashboardLayout>
@@ -17,6 +34,12 @@ const authStore = useAuthStore()
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="bg-white mt-10 px-4 py-6 rounded shadow-md overflow-x-auto">
         <p>welcome {{ authStore.auth?.name }}</p>
+      </div>
+    </div>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="bg-white mt-10 px-4 py-6 rounded shadow-md overflow-x-auto">
+        <apexchart type="bar" :options="options" :series="series"></apexchart>
       </div>
     </div>
   </DashboardLayout>
